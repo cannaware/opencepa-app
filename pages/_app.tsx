bbec7a89e0f4781aaba3b59f358cbc7d3fc5d8dc
cannawaredev <cannaware.io@gmail.com>
2021-05-03 01:56:19 -0300

    add custom _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { FC } from 'react';
 import type { AppProps } from 'next/app';
-import 'tailwindcss/tailwind.css';
+// import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
