b13bf08e3c660c7b9988c6dc9d55f5c86bcf94d2
cannawaredev <cannaware.io@gmail.com>
2021-05-03 02:39:03 -0300

    update layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -14,7 +14,7 @@ const Layout: React.FC = ({ children }) => {
         <meta name="msapplication-config" content="/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#059669" />
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#059669" />
 
         <link rel="apple-touch-icon" href="/static/icons/touch-icon-iphone.png" />
         <link rel="apple-touch-icon" sizes="152x152" href="/images/icons/icon-152x152.png" />
--- pages/acerca.tsx
+++ pages/acerca.tsx
@@ -1,28 +1,10 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
-import Head from 'next/head';
 
 const AcercaPage: NextPage = () => {
   return (
     <main>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>OpenCepa</title>
-
-        <link rel="manifest" href="/manifest.json" />
-        <link href="/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
-        <link href="/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#059669" />
-      </Head>
-      <h1>Info NextJS</h1>
+      <h1>Acerca OpenCepa</h1>
       <p>
         <Link href="/">
           <a>Volver al Inicio</a>
